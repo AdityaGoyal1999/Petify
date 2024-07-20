923596c8cfca537f4d040d54d00dab1b86de397d
Aditya Goyal <adityagoyaldoon@gmail.com>
2024-07-20 08:34:31 -0400

    About Us section updated

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -2,19 +2,39 @@ import { Box, Grid, Typography } from '@mui/joy'
 import React from 'react'
 import pawAnimation from "../assets/paw-animation.gif"
 import girlOnComputer from "../assets/girl-on-computer.gif"
+import { FEATURES } from '../constants'
+
 
 const AboutUs = () => {
   return (
-    <Box 
-        className="bg-gradient-to-r from-indigo-500"
-        sx={{ height: '600px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <Box sx={{ width: "80%", height: "400px", backgroundColor: 'white'}} className="shadow-lg rounded-lg flex-column max-container padding-container flexCenter">
-          <Box className="flexCenter flex-col">
-            <Typography level="h1" className="border border-red-500">About Us</Typography>
-            <Typography>Choose your next pet from the comfort of your home with us.</Typography>
-          </Box>
-        </Box>
+  <Box className="rounded-lg flex-column flexCenter max-container padding-container pb-32">
+      <Box className="flexCenter flex-col">
+        <h1 className="bold-52 l:bold-64">About Us</h1>
+        
+        <ul className="mt-10 grid md:grid-cols-2 gap-16 lg:mt-20 lg:gap-32">
+          {FEATURES.map((feature) => (
+            <FeatureItem title={feature.title} description={feature.description} key={feature.title} />
+          ))}
+        </ul>
+      </Box>
     </Box>
+  )
+}
+
+const FeatureItem = ({title, description, key}) => {
+  return (
+    <li className="flex flex-col items-start">
+      <Box className="rounded-full p-4 lg:p-7 bg-blue-100">
+        <img
+          src={pawAnimation}
+          alt="Paw Icon"
+          width={28}
+          height={28}
+        ></img>
+      </Box>
+      <h3 className="bold-20 lg:bold-32 mt-5 capitalize">{title}</h3>
+      <p className="regular-16 mt-5 bg-white text-gray-30 lg:mt-[30px] lg:bg-none text-wrap">{description}</p>
+    </li>
   )
 }
 
---
+++ frontend/src/constants/index.js
@@ -0,0 +1,20 @@
+
+
+export const FEATURES = [
+    {
+        title: 'Locate pets near you',
+        description: 'Search for the pet you\'re looking for and locate them with our interactive map.'
+    },
+    {
+        title: 'Message buyer or seller',
+        description: 'We provide a way for you to connect with the buyer/seller by providing an in-app messaging feature. Share much more than just text, you can share images etc.'
+    },
+    {
+        title: 'Post About Your Pet',
+        description: 'Are you looking for the right person to adopt your pet? Post about them on the portal and get messages from them so you can select the right person for your pet.'
+    },
+    {
+        title: 'Pet FAQs',
+        description: 'Learn about the Frequently Asked Questions when it comes to pet adoption or any other question you have on this matter.'
+    }
+]
